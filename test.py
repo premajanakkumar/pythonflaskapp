2cd2456bf329af66fdb7065c62ccf585596cf45e
premajanakkumar <53522810+premajanakkumar@users.noreply.github.com>
2024-02-27 17:42:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,6 @@
-#!/usr/bin/env python
-import unittest
-import app
+from app import *
 
-class TestHello(unittest.TestCase):
-
-    def setUp(self):
-        app.app.testing = True
-        self.app = app.app.test_client()
-
-    def test_hello(self):
-        rv = self.app.get('/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
-    def test_hello_hello(self):
-        rv = self.app.get('/hello/')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'Hello World!\n')
-
-    def test_hello_name(self):
-        name = 'Simon'
-        rv = self.app.get(f'/hello/{name}')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
-
-if __name__ == '__main__':
-    unittest.main()
-TEST_CASES
-chmod +x test.py
+def test_Home():
+    response = app.test_client().get('/')
+    assert b"Welcome to Jenkins Tutorials" in response.data
+    assert response.status_code == 200
