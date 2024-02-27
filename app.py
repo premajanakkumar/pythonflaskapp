1b4ba7a42f892ad080509ee2ecbb6b4a6fbd1672
premajanakkumar <53522810+premajanakkumar@users.noreply.github.com>
2024-02-27 17:42:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,10 @@
-#!/usr/bin/env python
-from flask import Flask
+from flask import *
+
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello/')
-def hello_world():
-    return 'Hello World!\n'
-
-@app.route('/hello/<username>') # dynamic route
-def hello_user(username):
-    return 'Why Hello %s!\n' % username
+def home():
+    return "<h1>Welcome to Jenkins Tutorials<h1/>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')     # open for everyone
+    app.run(debug=True,host='0.0.0.0'
